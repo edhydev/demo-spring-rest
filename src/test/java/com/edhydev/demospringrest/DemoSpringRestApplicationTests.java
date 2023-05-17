4ead583cc2f1ce0d8495de98462fe26960b14e9f
edhydev <edgar.jimenez.dev@gmail.com>
2023-05-16 21:39:32 -0600

    initial commit

---
+++ src/test/java/com/edhydev/demospringrest/DemoSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edhydev.demospringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
